a56efcd8d3c25a5a07db6257be787ac4a05d8a5c
Dicon35 <123318860+Dicon35@users.noreply.github.com>
2024-11-11 17:06:54 +0000

    Update ex_part1.py

--- ex_part1.py
+++ ex_part1.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-# 1. Виконання GET-запиту за допомогою requests
 url = 'https://www.example.com'
 response = requests.get(url)
 
@@ -12,4 +11,3 @@ else:
     print(f"Помилка при виконанні запиту: {response.status_code}")
     exit()
 
-# 2. Парсинг HTML за допомогою BeautifulSoup
